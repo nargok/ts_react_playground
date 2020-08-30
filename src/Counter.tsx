f179dbba1549d9ff3564cfd1bc561344e842948f
nargok <nargok@gmail.com>
2020-08-30 16:04:07 +0900

    use state

---
+++ src/Counter.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+const Counter: React.FC = () => {
+  const [value, setValue] = useState<number>(0);
+
+  const increment = () => {
+    setValue((previousState) => previousState + 1);
+    // setValue(value + 1);
+  };
+
+  const decrement = () => {
+    setValue((previousState) => previousState - 1);
+    // setValue(value - 1);
+  };
+
+  return (
+    <div>
+      <div>value: {value}</div>
+      <button onClick={increment}>+1</button>
+      <button onClick={decrement}>-1</button>
+    </div>
+  );
+};
+
+export default Counter;
