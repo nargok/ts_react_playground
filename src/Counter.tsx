8532f76b9b768d0a3c6866b85d3fc829eb248cb7
nargok <nargok@gmail.com>
2020-08-30 16:15:00 +0900

    useRef and UseEffect

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 const Counter: React.FC = () => {
   const [value, setValue] = useState<number>(0);
@@ -13,11 +13,17 @@ const Counter: React.FC = () => {
     // setValue(value - 1);
   };
 
+  const renderTimes = useRef<number>(0);
+  useEffect(() => {
+    renderTimes.current = renderTimes.current + 1;
+  });
+
   return (
     <div>
       <div>value: {value}</div>
       <button onClick={increment}>+1</button>
       <button onClick={decrement}>-1</button>
+      <div>This component was re-rendered {renderTimes.current} times</div>
     </div>
   );
 };
