cf8a9e31d9209d33b3df12738de33d6407e0dc1f
nargok <nargok@gmail.com>
2020-08-30 16:39:20 +0900

    useRef 2nd

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -18,12 +18,24 @@ const Counter: React.FC = () => {
     renderTimes.current = renderTimes.current + 1;
   });
 
+  const inputRef = useRef<HTMLInputElement>(null!); // nonNull Assertion operator 方法その3 TSにその状態を使用する時にnullじゃないことを伝える
+  const focusInput = () => {
+    // const current = inputRef.current;
+    // if (current != null) current.focus(); // 方法その1
+
+    // inputRef.current?.focus(); // 方法その2
+
+    inputRef.current.focus();
+  };
+
   return (
     <div>
       <div>value: {value}</div>
       <button onClick={increment}>+1</button>
       <button onClick={decrement}>-1</button>
       <div>This component was re-rendered {renderTimes.current} times</div>
+      <input type="text" ref={inputRef} />
+      <button onClick={focusInput}>Click me</button>
     </div>
   );
 };
